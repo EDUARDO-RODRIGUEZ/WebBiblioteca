f1a1ba9e87f3ea1d6a2833a19ca425c40c4f1752
Eduardo <jorge.ecrg@gmail.com>
2022-11-19 16:25:09 -0400

    Login Face Complete

--- src/pages/auth/register/index.jsx
+++ src/pages/auth/register/index.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { Link } from 'react-router-dom';
 import svgwave from '../../../assets/wave.svg';
 import { usuarioContext } from '../../../context/usuarioContext';
 
@@ -74,6 +75,12 @@ const Register = () => {
                 name={"perfil"}
                 accept="image/*"
               />
+            </div>
+
+            <div className={"text-center my-2 hover:underline "}>
+              <Link to={"/auth/login"}>
+                <small>¿Ya tienes cuenta? Login</small>
+              </Link>
             </div>
 
             <div className={"px-4 mt-5"}>
